81c44cdfdf8b24806d6128166b8f6fe2a5adde17
Cultuly <Cultuly@gmail.com>
2025-05-05 23:35:38 +0500

    Added projectiles classes

---
+++ HauntedHouse/Models/Projecitles/ProjectileData.cs
@@ -0,0 +1,10 @@
+namespace HauntedHouse;
+
+public sealed class ProjectileData
+{
+    public Vector2 Position {get; set;}
+    public float Rotation {get; set;}
+    public int Speed {get; set;}
+    public int Damage {get; set;}
+    public float Lifespan {get; set;}
+}
--- HauntedHouse/obj/Debug/net8.0/HauntedHouse.AssemblyInfo.cs
+++ HauntedHouse/obj/Debug/net8.0/HauntedHouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HauntedHouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469582d962b05700fdbf25ccb72617e09a328e2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ad387cc230e8c5f96e16877da5b0999782f18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HauntedHouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HauntedHouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
